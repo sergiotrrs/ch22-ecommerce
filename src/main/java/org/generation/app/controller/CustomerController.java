86c713b312517b83ae8a1b3a6268cba63c5341ed
Sergio Torres <sergio.torres@idr.org.mx>
2023-02-28 13:11:29 -0600

    ci: creación del CustomerService

---
+++ src/main/java/org/generation/app/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package org.generation.app.controller;
+
+import java.util.List;
+
+import org.generation.app.model.Customer;
+import org.generation.app.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/customers")
+public class CustomerController {
+
+	@Autowired
+	ICustomerService customerService;
+	
+	public List<Customer> getAllCustomers(){
+		return customerService.getAllCustomers();
+		
+	}
+	
+}
