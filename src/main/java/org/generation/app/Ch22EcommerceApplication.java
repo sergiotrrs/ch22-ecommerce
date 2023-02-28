ead69a0fafb77c4fef3d695e371a94a45e7101ff
Sergio Torres <sergio.torres@idr.org.mx>
2023-02-28 11:11:13 -0600

    feat: inicio del proyecto en springboot

---
+++ src/main/java/org/generation/app/Ch22EcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.generation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch22EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch22EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/app/Ch22EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch22EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
