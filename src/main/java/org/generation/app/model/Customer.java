9038ab8fe0633262659441475e139bc8a75d7f74
Sergio Torres <sergio.torres@idr.org.mx>
2023-02-28 12:32:45 -0600

    ci: creacion de la entidad Customer

--- src/main/java/org/generation/app/model/Customer.java
+++ src/main/java/org/generation/app/model/Customer.java
@@ -1,5 +1,30 @@
 package org.generation.app.model;
 
-public class Customer {
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Data //Genera los setters and getters con Lombok
+@Table( name= "customer")
+public class Customer {	
+	public static final int FIELD_MAX_LENGTH = 160;
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	@Column(name="id_customer")
+	private long idCustomer;
+	@Column(name="first_name",nullable = false ,length= FIELD_MAX_LENGTH)
+	private String firstName;
+	@Column(name="last_name", length= FIELD_MAX_LENGTH)
+	private String lastName;
+	@Column(name="email",nullable = false, updatable=false, unique = true, length= FIELD_MAX_LENGTH )
+	private String email;
+	@Column(name="password", length= FIELD_MAX_LENGTH)
+	private String password;
+	@Column(name="avatar", length= FIELD_MAX_LENGTH)
+	private String avatar;
+	@Column(name="is_active")
+	private boolean active;
+	
+	
 }
