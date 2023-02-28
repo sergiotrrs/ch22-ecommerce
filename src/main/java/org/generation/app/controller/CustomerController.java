4cdbc472ac4c881ebf6183050fd97e4e77ea0e65
Sergio Torres <sergio.torres@idr.org.mx>
2023-02-28 14:30:32 -0600

    ci: consumiendo la excepsion IllegalStateException

--- src/main/java/org/generation/app/controller/CustomerController.java
+++ src/main/java/org/generation/app/controller/CustomerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.generation.app.model.Customer;
 import org.generation.app.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,8 +30,14 @@ public class CustomerController {
 	}
 	
 	@GetMapping("{id}") //localhost:8080/api/cusytomers/2
-	public Customer getCustomerById(@PathVariable("id") long idCustomer) {
-		return customerService.getCustomerById(idCustomer);
+	public ResponseEntity<?> getCustomerById(@PathVariable("id") long idCustomer) {
+		try {
+			return new ResponseEntity<Customer>(
+					customerService.getCustomerById(idCustomer), 
+					HttpStatus.OK);													
+		} catch (IllegalStateException e) {
+			return new ResponseEntity<String>(e.getMessage() , HttpStatus.NOT_FOUND );
+		}
 	}
 	
 	
