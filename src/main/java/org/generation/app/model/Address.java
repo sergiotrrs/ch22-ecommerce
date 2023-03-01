fa723c67d6ebf57b153cab541cadd06ffe667a7c
Sergio Torres <sergio.torres@idr.org.mx>
2023-03-01 11:19:07 -0600

    ci: entity address

---
+++ src/main/java/org/generation/app/model/Address.java
@@ -0,0 +1,30 @@
+package org.generation.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data //Genera los setters and getters con Lombok
+@Table( name= "address")
+public class Address {
+
+	public static final int FIELD_MAX_LENGTH = 160;
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	@Column(name="id_address")
+	private long idAddress;
+	@Column(name="address",nullable = false ,length= FIELD_MAX_LENGTH)
+	private String address;
+	@Column(name="zip_code", length= FIELD_MAX_LENGTH)
+	private String zipCode;
+	@Column(name="city", length= FIELD_MAX_LENGTH)
+	private String city;
+	
+	
+	
+}
