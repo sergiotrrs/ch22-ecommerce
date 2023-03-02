c16764332cf9916a07bd4332529609a79fdbf438
Sergio Torres <sergio.torres@idr.org.mx>
2023-03-02 10:21:33 -0600

    ci: buscar direcciones por id cliente

--- src/main/java/org/generation/app/repository/IAddressRepository.java
+++ src/main/java/org/generation/app/repository/IAddressRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface IAddressRepository extends CrudRepository<Address, Long>  {
 	
-	//Buscar todas las direcciones por el id del cliente.
+	// Buscar todas las direcciones por el id del cliente.
 		List<Address> findAllByFkIdCustomerIdCustomer(long idCustomer);
+	// Buscar todas las direcciones por el email del cliente
+		List<Address> findAllByFkIdCustomerEmail(String email);
+	// Buscar todas las direcciones con el misco código postal	
 		List<Address> findAllByZipCode(String ZipCode);
 
 }
